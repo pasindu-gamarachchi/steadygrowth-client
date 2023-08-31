6b2df8cc1112816aba340212cfca1406b35bc8d0
pasindu-gamarachchi <pgamarachchi@gmail.com>
2023-08-31 16:54:10 -0400

    Edit form cleanup

--- src/components/FormRow/FormRow.js
+++ src/components/FormRow/FormRow.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import "./FormRow.scss";
+
 
 const FormRow = ({stock_symb, purchase_date, purchase_price, purchase_shares}) => {
     return (
-        <div>
+        <div className='RowContainer'>
             <div>{stock_symb}</div>
             <div>{purchase_date}</div>
             <div>{purchase_price}</div>
