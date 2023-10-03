01f030e86ae4e13fd9960a9b9900d956f4907727
pasindu-gamarachchi <pgamarachchi@gmail.com>
2023-10-02 20:11:30 -0400

    Confirm modal functional and styled

--- src/components/ConfirmModal/ConfirmModal.js
+++ src/components/ConfirmModal/ConfirmModal.js
@@ -1,78 +1,83 @@
-import "./ConfirmModal.scss"
+import "./ConfirmModal.scss";
 import closeIcon from "../../assets/icons/close-24px.svg";
-import { Link, useNavigate } from 'react-router-dom';
-import axios from "axios";
 
+const ConfirmModal = ({
+  modalInfo,
+  deleteConfirmed,
+  updateConfirmed,
+  modalAction,
+  setshowModal,
+}) => {
+  function closeModal(event) {
+    setshowModal(false);
+  }
 
-const ConfirmModal = ({modalInfo, url, deleteConfirmed, updateConfirmed, modalAction, setshowModal}) => {
+  return (
+    <section className="delete">
+      <div className="delete__section">
+        <div className="delete__text-section">
+          <div className="delete__close">
+            <img
+              onClick={() => {
+                closeModal();
+              }}
+              className="delete__close-icon"
+              alt="x-symbol"
+              src={closeIcon}
+            />
+          </div>
 
-
-    const navigate = useNavigate();
-
-    function closeModal(event) {
-        setshowModal(false);
-        console.log('Close modal clicked.')
-    }
-
-    return (
-        <section
-            className="delete">
-            <div className="delete__section">
-                <div className="delete__text-section">
-                    <div className="delete__close"
-                    >
-                        <img
-                            onClick={() => { closeModal() }}
-                            className="delete__close-icon"
-                            alt="x-symbol"
-                            src={closeIcon} />
-                    </div>
-
-                    <h1 className="delete__heading">{modalInfo.heading}</h1>
-                    <span className="delete__text">{modalInfo.text}</span>
-                </div>
-                <div className="delete__ctas">
-                    {modalAction==="Update"?
-                    <div>
-                        <div className="delete__button-box">
-
-                            <Link to={url}
-                                onClick={() => { closeModal() }}
-                            >
-                                <span className="delete__button delete__button--outline">Cancel</span>
-                            </Link>
-                        </div>
-                        <div className="delete__button-box">
-                            <Link to={url} onClick={()=>{updateConfirmed()}}
-                            >
-                                <span className="delete__button">Confirm</span>
-                            </Link>
-                        </div>
-                    </div>:
-                    <div>
-                        <div className="delete__button-box">
-
-                            <Link to={url}
-                                onClick={() => { closeModal() }}
-                            >
-                                <span className="delete__button delete__button--outline">Cancel</span>
-                            </Link>
-                        </div>
-                        <div className="delete__button-box">
-                            <Link to={url} onClick={()=>{deleteConfirmed()}}
-                            >
-                                <span className="delete__button">Delete</span>
-                            </Link>
-                        </div>
-                    </div>
-                    }   
-                </div>
+          <h1 className="delete__heading">{modalInfo.heading}</h1>
+          <span className="delete__text">{modalInfo.text}</span>
+        </div>
+        <div>
+          {modalAction === "Update" ? (
+            <div className="delete__ctas">
+              <div
+                className="delete__button-box"
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                <span className="delete__button delete__button--outline">
+                  Cancel
+                </span>
+              </div>
+              <div
+                className="delete__button-box"
+                onClick={() => {
+                  updateConfirmed();
+                }}
+              >
+                <span className="delete__button">Confirm</span>
+              </div>
             </div>
-        </section >
-    );
+          ) : (
+            <div className="delete__ctas">
+              <div
+                className="delete__button-box"
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                <span className="delete__button delete__button--outline">
+                  Cancel
+                </span>
+              </div>
+              <div
+                className="delete__button-box"
+                onClick={() => {
+                  deleteConfirmed();
+                }}
+              >
+                <span className="delete__button">Delete</span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
-
-
-
 
 export default ConfirmModal;
